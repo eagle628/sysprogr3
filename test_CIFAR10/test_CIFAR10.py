7049fc61b0bc157696e422ea78f27c80f8f873f3
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-10-30 19:37:05 +0900

    test CIFAR10

---
+++ test_CIFAR10/test_CIFAR10.py
@@ -0,0 +1,130 @@
+#!python3.6.6
+import random
+import numpy as np
+import chainer
+import matplotlib.pyplot as plt
+from chainer.datasets import cifar
+from chainer.datasets import split_dataset_random
+from chainer import iterators
+import chainer.links as L
+import chainer.functions as F
+from chainer import optimizers
+from chainer import training
+from chainer.training import extensions
+from chainer.cuda import to_cpu
+
+#############################################################
+def reset_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    if chainer.cuda.available:
+        chainer.cuda.cupy.random.seed(seed)
+
+class DeepCNN(chainer.ChainList):
+
+    def __init__(self, n_output):
+        super(DeepCNN, self).__init__(
+            ConvBlock(64),
+            ConvBlock(64, True),
+            ConvBlock(128),
+            ConvBlock(128, True),
+            ConvBlock(256),
+            ConvBlock(256),
+            ConvBlock(256),
+            ConvBlock(256, True),
+            LinearBlock(),
+            LinearBlock(),
+            L.Linear(None, n_output)
+        )
+
+    def __call__(self, x):
+        for f in self:
+            x = f(x)
+        return x
+
+class ConvBlock(chainer.Chain):
+
+    def __init__(self, n_ch, pool_drop=False):
+        w = chainer.initializers.HeNormal()
+        super(ConvBlock, self).__init__()
+        with self.init_scope():
+            self.conv = L.Convolution2D(None, n_ch, 3, 1, 1, nobias=True, initialW=w)
+            self.bn = L.BatchNormalization(n_ch)
+        self.pool_drop = pool_drop
+
+    def __call__(self, x):
+        h = F.relu(self.bn(self.conv(x)))
+        if self.pool_drop:
+            h = F.max_pooling_2d(h, 2, 2)
+            h = F.dropout(h, ratio=0.25)
+        return h
+
+class LinearBlock(chainer.Chain):
+
+    def __init__(self, drop=False):
+        w = chainer.initializers.HeNormal()
+        super(LinearBlock, self).__init__()
+        with self.init_scope():
+            self.fc = L.Linear(None, 1024, initialW=w)
+        self.drop = drop
+
+    def __call__(self, x):
+        h = F.relu(self.fc(x))
+        if self.drop:
+            h = F.dropout(h)
+        return h
+
+def train(network_object, batchsize=128, gpu_id=0, max_epoch=20, train_dataset=None, valid_dataset=None, test_dataset=None, postfix='', base_lr=0.01, lr_decay=None):
+
+    # 1. Dataset
+    if train_dataset is None and valid_dataset is None and test_dataset is None:
+        train_val, test = cifar.get_cifar10()
+        train_size = int(len(train_val) * 0.9)
+        train, valid = split_dataset_random(train_val, train_size, seed=0)
+    else:
+        train, valid, test = train_dataset, valid_dataset, test_dataset
+
+    # 2. Iterator
+    train_iter = iterators.SerialIterator(train, batchsize)
+    valid_iter = iterators.SerialIterator(valid, batchsize, False, False)
+
+    # 3. Model
+    net = L.Classifier(network_object)
+
+    # 4. Optimizer
+    optimizer = optimizers.MomentumSGD(lr=base_lr).setup(net)
+    optimizer.add_hook(chainer.optimizer.WeightDecay(0.0005))
+
+    # 5. Updater
+    updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
+
+    # 6. Trainer
+    trainer = training.Trainer(updater, (max_epoch, 'epoch'), out='{}_cifar10_{}result'.format(network_object.__class__.__name__, postfix))
+
+    # 7. Trainer extensions
+    trainer.extend(extensions.LogReport())
+    trainer.extend(extensions.observe_lr())
+    trainer.extend(extensions.Evaluator(valid_iter, net, device=gpu_id), name='val')
+    trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'elapsed_time', 'lr']))
+    trainer.extend(extensions.PlotReport(['main/loss', 'val/main/loss'], x_key='epoch', file_name='loss.png'))
+    trainer.extend(extensions.PlotReport(['main/accuracy', 'val/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
+    if lr_decay is not None:
+        trainer.extend(extensions.ExponentialShift('lr', 0.1), trigger=lr_decay)
+    trainer.run()
+    del trainer
+
+    # 8. Evaluation
+    test_iter = iterators.SerialIterator(test, batchsize, False, False)
+    test_evaluator = extensions.Evaluator(test_iter, net, device=gpu_id)
+    results = test_evaluator()
+    print('Test accuracy:', results['main/accuracy'])
+
+    return net
+#######################################################
+# set random seed
+reset_seed(0)
+
+chainer.cuda.set_max_workspace_size(256 * 1024 * 1024)
+chainer.config.autotune = True
+
+model = train(DeepCNN(10), max_epoch=100, base_lr=0.1, lr_decay=(30, 'epoch'))
