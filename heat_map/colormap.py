9864a6daca7266236f2040e83dadb9c809b78c71
darma <darma@example.com>
2019-01-30 16:59:02 +0900

    modify gausian->circle , add colorbar

--- heat_map/colormap.py
+++ heat_map/colormap.py
@@ -46,7 +46,8 @@ class Heatmapimage :
         self.resize_scale = quality
         self.image_size = list(self.original_map.shape[0:2])
         self.colormap_size = [int(x / quality) for x in self.image_size]
-        self.color_weight = np.zeros(self.colormap_size+[1,], np.uint8)
+        self.color_weight = np.zeros(self.colormap_size+[1,], np.uint16)
+        self.blank_image = np.zeros(self.image_size+[1,], np.uint8)
 
     def __gaussian_2d(self,x,y):
         x_c = np.array([x, y]) - self.mu
@@ -72,18 +73,44 @@ class Heatmapimage :
         Z = self.__gaussian_2d_matrix(pos,var)
         Z = (Z * 255 * weight) / np.amax(Z)   
         self.color_weight[:,:,0] = self.color_weight[:,:,0] + Z
-        
+    
+    def add_circle(self,pos,shape,darkness):
+        self.blank_image = cv2.ellipse(self.blank_image,((pos[0],pos[1]),(shape[0],shape[1]),360),(darkness,darkness,darkness), -1)       
 
-
-    def export_heatmap(self, alpha,color_type):
-        self.color_weight = (self.color_weight / np.amax(self.color_weight) ) * 255
-        self.color_weight = self.color_weight.astype(np.uint8)
-        self.image_size[0],self.image_size[1] = self.image_size[1],self.image_size[0]
-        self.image_size = tuple(self.image_size)
-        self.color_weight = cv2.resize(self.color_weight, self.image_size)
-        color_map = cv2.applyColorMap(self.color_weight, color_type)
+    def export_heatmap(self, alpha,color_type,blur=10):
+        # self.color_weight = (self.color_weight / np.amax(self.color_weight) ) * 255
+        # self.color_weight = self.color_weight.astype(np.uint8)
+        # self.image_size[0],self.image_size[1] = self.image_size[1],self.image_size[0]
+        # self.image_size = tuple(self.image_size)
+        # self.color_weight = cv2.resize(self.color_weight, self.image_size)
+        # color_map = cv2.applyColorMap(self.color_weight, color_type)
+        # blended = cv2.addWeighted(self.original_map, 1 - alpha, color_map, alpha, 0)
+        color_map = cv2.applyColorMap(self.blank_image, color_type)
+        color_map = cv2.blur(color_map,(blur,blur))
         blended = cv2.addWeighted(self.original_map, 1 - alpha, color_map, alpha, 0)
         return blended
+
+    def __get_gradation_2d(self, start, stop, width, height, is_horizontal):
+        if is_horizontal:
+            return np.tile(np.linspace(start, stop, width), (height, 1))
+        else:
+            return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+    def __get_gradation_3d(self, width, height, start_list, stop_list, is_horizontal_list):
+        result = np.zeros((height, width, len(start_list)), dtype=np.uint8)
+        for i, (start, stop, is_horizontal) in enumerate(zip(start_list, stop_list, is_horizontal_list)):
+            result[:, :, i] = self.__get_gradation_2d(start, stop, width, height, is_horizontal)
+        return result
+
+    def export_heatmap_with_colorbar(self, alpha,color_type,blur=10):
+        color_map = cv2.applyColorMap(self.blank_image, color_type)
+        color_map = cv2.blur(color_map,(blur,blur))
+        blended = cv2.addWeighted(self.original_map, 1 - alpha, color_map, alpha, 0)
+        # array = self.__get_gradation_3d(512, 256, (0, 0, 0), (255, 255, 255), (True, True, True))
+        gradation_array = self.__get_gradation_3d(50, self.image_size[0], (255, 255, 255), (0, 0, 0), (False, False, False))
+        color_bar = cv2.applyColorMap(gradation_array, color_type)
+        heatmap = cv2.hconcat([blended, color_bar])
+        return heatmap
  
 
 
